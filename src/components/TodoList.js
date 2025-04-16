b71ba36498968dce8ca062af9b5fde4e78348a0c
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-16 13:40:14 +0530

    second commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,7 @@ const TodoList = ({ todos, handleComplete }) => {
     <div>
       {todos.map((todo, index) => (
         <li key={index}>
-          {todo.todo} {!todo.state && <button onClick={()=>handleComplete(index)}>complete</button>}
+          {todo.todo} {!todo.state && <button onClick={()=>handleComplete(index)}>Complete</button>}
         </li>
       ))}
     </div>
